4bf0c8ad2b951b6e4e465f4ca14b7b9a82510c97
Nathalie <Nathalie@172.32.8.25>
2015-07-11 10:54:27 -0500

    Imagen principal y ubicación de autores

--- app/views/mypage/index.php
+++ app/views/mypage/index.php
@@ -44,7 +44,7 @@
 					<section id="banner">
 						<div class="inner">
 							<h2>Magna</h2>
-							<p>Desarrollo Web <a href="http://html5up.net">HTML5 UP</a>.</p>
+							<p>Desarrollo Web.</p>
 							
 						</div>
 						<a href="#one" class="more scrolly">Learn More</a>
@@ -54,8 +54,7 @@
 					<section id="one" class="wrapper style1 special">
 						<div class="inner">
 							<header class="major">
-								<h2>Arcu aliquet vel lobortis ata nisl<br />
-								eget augue amet aliquet nisl cep donec</h2>
+								<h2>¿Quienes somos?</h2>
 								<p>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend<br />
 								fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</p>
 							</header>
@@ -71,22 +70,19 @@
 					<section id="two" class="wrapper alt style2">
 						<section class="spotlight">
                                                     <div class="image"><img src="libs/img/pic01.jpg" alt="" /></div><div class="content">
-								<h2>Magna primis lobortis<br />
-								sed ullamcorper</h2>
+								<h2>Jorge Andrés Bedoya Hernández</h2>
 								<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
 							</div>
 						</section>
 						<section class="spotlight">
 							<div class="image"><img src="libs/img/pic02.jpg" alt="" /></div><div class="content">
-							<h2>Tortor dolore feugiat<br />
-								elementum magna</h2>
+							<h2>Nataly Castrillón Charari</h2>
 								<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
 							</div>
 						</section>
 						<section class="spotlight">
 							<div class="image"><img src="libs/img/pic03.jpg" alt="" /></div><div class="content">
-								<h2>Augue eleifend aliquet<br />
-								sed condimentum</h2>
+								<h2>Camilo Taborda Zuluaga</h2>
 								<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
 							</div>
 						</section>
